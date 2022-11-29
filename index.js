888c76dcf8c97233763d4729b596e0f7c3dbeae8
Seda Ünal <sedaylmz06@outlook.com>
2022-11-30 00:37:01 +0300

    array map

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((tutorial) => {
+    console.log(
+      tutorial
+        .split(" ")
+        .map((splitted) => {
+          return (
+            splitted.charAt(0).toUpperCase() + splitted.slice(1).toLowerCase()
+          );
+        })
+        .join(" ")
+    );
+  });
+};
+
+titleCased();
+
+// const toTitleCase = (phrase) => {
+//   return phrase
+//     .toLowerCase()
+//     .split(" ")
+//     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+//     .join(" ");
+// };
+
+// let result2 = toTitleCase("maRy hAd a lIttLe LaMb");
+// console.log(result2);
